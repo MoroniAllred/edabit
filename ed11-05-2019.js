2c753437847451820ebffa6e2c7f8bc71ff8c499
Moroni Allred <moroniallred@gmail.com>
2019-11-05 18:20:11 -0700

    ed11-05-2019

--- ed11-05-2019.js
+++ ed11-05-2019.js
@@ -1,5 +1,19 @@
-function hello() {
-	return "hello edabit.com"
+function isInRange(num, range) {
+	if(num > range.max || num <range.min){
+		return false
+	}
+	else{
+		return true
+	}
 }
 
-Test.assertEquals(hello(), "hello edabit.com", "Did you *return* the result?")
+Test.assertEquals(isInRange(4, { min: 0, max: 5 }), true)
+Test.assertEquals(isInRange(4, { min: 4, max: 5 }), true)
+Test.assertEquals(isInRange(4, { min: 0, max: 4 }), true)
+Test.assertEquals(isInRange(4, { min: 6, max: 10 }), false)
+Test.assertEquals(isInRange(11, { min: 6, max: 10 }), false)
+Test.assertEquals(isInRange(5, { min: 5, max: 5 }), true)
+Test.assertEquals(isInRange(1.5, { min: 1.25, max: 1.75 }), true)
+Test.assertEquals(isInRange(1.1, { min: 1.25, max: 1.75 }), false)
+Test.assertEquals(isInRange(1.8, { min: 1.25, max: 1.75 }), false)
+Test.assertEquals(isInRange(-1, { min: -1, max: 1 }), true)
