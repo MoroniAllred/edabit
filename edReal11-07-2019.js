9a3bf19d0b82357076ff58790614f6131ac73590
Moroni Allred <moroniallred@gmail.com>
2019-11-07 18:08:29 -0700

    edReally11-07-2019

---
+++ edReal11-07-2019.js
@@ -0,0 +1,10 @@
+function volPizza(radius, height) {
+	return Math.round(radius * radius * height * 3.14159)
+}
+
+Test.assertEquals(volPizza(1, 1), 3, "Example #1")
+Test.assertEquals(volPizza(7, 2), 308, "Example #2")
+Test.assertEquals(volPizza(10, 2.5), 785, "Example #3")
+Test.assertEquals(volPizza(15, 1.3), 919)
+Test.assertEquals(volPizza(20, 1), 1257)
+Test.assertEquals(volPizza(13, 2), 1062)
